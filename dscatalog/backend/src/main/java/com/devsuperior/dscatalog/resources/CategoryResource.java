6d9e8a9d1d942372ab01d46b087642727a4f6c85
joabehenrique <henriquecidoz@hotmail.com>
2022-02-10 10:54:57 -0300

    feat: adicionado busca de todas as categorias

--- dscatalog/backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ dscatalog/backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,6 +1,8 @@
 package com.devsuperior.dscatalog.resources;
 
 import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +15,13 @@ import java.util.List;
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
+    @Autowired
+    public CategoryService service;
+
     @GetMapping
     public ResponseEntity<List<Category>> findAll(){
-        List<Category> list = new ArrayList<>();
+        List<Category> list = service.findAll();
 
-        list.add(new Category(1L, "Eticos"));
-        list.add(new Category(2L, "Bonificados"));
-
-        return ResponseEntity.ok(list);
+        return ResponseEntity.ok().body(list);
     }
 }
