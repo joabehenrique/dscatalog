945d503571f2734d935f7897947be5d1242f5985
joabehenrique <henriquecidoz@hotmail.com>
2023-03-27 11:52:38 -0300

    refactor: refactored the pagination

--- dscatalog/backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ dscatalog/backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -13,6 +13,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,7 +30,7 @@ public class ProductService {
     public CategoryRepository categoryRepository;
 
     @Transactional(readOnly = true)
-    public Page<ProductDTO> findAllPaged(PageRequest page){
+    public Page<ProductDTO> findAllPaged(Pageable page){
         Page<Product> list = repository.findAll(page);
         return list.map(ProductDTO::new);
     }
